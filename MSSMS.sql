e40473c0f7a2e8bce917fb85d318659bf9bce696
arturp999 <arturpe95@gmail.com>
2021-07-05 22:21:26 +0100

    First

---
+++ MSSMS.sql
@@ -0,0 +1,17 @@
+
+--CREATE TABLE Movies(
+--id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--[Name] VARCHAR(200),
+--Rating INT,
+--Release_Year INT,
+--Movie_Img VARCHAR(100),
+--[Description] VARCHAR(250),
+--Movie_Trailer VARCHAR(200)
+--);
+
+--CREATE TABLE Genders(
+--id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--[Name] VARCHAR(50)
+--)
+
+CREATE
